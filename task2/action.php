440e385c9ded4bad08f3525aefecb59956664627
alex404tm <alex404tm@yandex.ru>
2022-05-27 15:23:44 +0600

    Home work1 - task 2

---
+++ task2/action.php
@@ -0,0 +1,42 @@
+<?php
+
+$name= '';
+$surname= '';
+
+if (isset($_GET["name"])){
+    $name = htmlspecialchars($_GET["name"]);
+    $name = trim($name);
+    $name = strtolower($name);
+    $name = strtolower($name);
+    $name = ucfirst($name);
+}
+
+if (isset($_GET["surname"])){
+    $surname = htmlspecialchars($_GET["surname"]);
+    $surname = trim($name);
+    $surname = strtolower($name);
+    $surname = strtolower($name);
+    $surname = ucfirst($name);
+}
+
+?>
+
+<!doctype html>
+ <html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ylab_HW_1.2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col-6 mt-3">
+                <?= "Привет, $name $surname."; ?>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
---
+++ task2/index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ylab_HW_1.2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <h2 class="mt-4">Home work 1.2 </h2>
+        <div class="col-3">
+            <form action="action.php" method="get">
+                <div class="form-group mb-3">
+                    <label for="exampleInputName">Имя</label>
+                    <input type="name" name="name" required class="form-control" id="exampleInputName" placeholder="Введите имя">
+                </div>
+                <div class="form-group mb-3">
+                    <label for="exampleInputSurname">Фамилия</label>
+                    <input type="name"  name="surname"  required class="form-control" id="exampleInputSurname" placeholder="Введите фамилию">
+                </div>
+                <button type="submit" class="btn btn-primary">Отправить</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
+
